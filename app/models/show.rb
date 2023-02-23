a76f3b94e41a2bd3d3dcee293d0d7562fad8906f
T-erry <terrywachuka10@gmail.com>
2023-02-23 15:10:44 +0300

    completed assignment

---
+++ app/models/show.rb
@@ -0,0 +1,29 @@
+class Show < ActiveRecord::Base
+    def self.highest_rating
+     self.maximum(:rating)
+    end 
+    def self.most_popular_show
+        highest_tv_rating = self.highest_rating
+        self.where(rating: highest_tv_rating).take
+    end
+    def self.lowest_rating
+     self.minimum(:rating)
+    end
+    def self.least_popular_show
+        lowest_streamed_show = self.lowest_rating
+        self.where(rating: lowest_streamed_show).take
+    end
+
+    def self.ratings_sum
+        self.sum(:rating)
+    end
+
+    def self.popular_shows
+        self.where("rating >= ?", 5)
+    end
+    
+    def self.shows_by_alphabetical_order
+        self.order(:name)
+    end
+        
+end
